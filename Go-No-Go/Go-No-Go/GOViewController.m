2a2d701fb2b9c06eb7d147879274076ab0f09399
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2016-01-11 17:53:14 -0500

    Added start button + explanation text

--- Go-No-Go/Go-No-Go/GOViewController.m
+++ Go-No-Go/Go-No-Go/GOViewController.m
@@ -8,7 +8,14 @@
 
 #import "GOViewController.h"
 
+float BUTTON_HEIGHT = 60.f;
+
 @interface GOViewController ()
+
+@property (strong, nonatomic) UITapGestureRecognizer *gestureRecognizer;
+
+@property (strong, nonatomic) UIButton *startButton;
+@property (strong, nonatomic) UILabel *explanationLabel;
 
 @end
 
@@ -20,15 +27,39 @@
     self.view.backgroundColor = [UIColor whiteColor];
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    // Game Explanation
+    self.explanationLabel = [[UILabel alloc] initWithFrame:CGRectMake(40, 200, CGRectGetWidth(self.view.frame) - 40, CGRectGetHeight(self.view.frame) / 2)];
+    [self.explanationLabel setCenter:self.view.center];
+    [self.explanationLabel setText:@"Welcome to the Go/No-Go test. \nOnce you start, you will be presented with a rectangle. When the rectangle turns green, press as soon as possible. When it turns blue, do not respond at all. The test will take 1 min."];
+    [self.explanationLabel setNumberOfLines:0];
+    [self.explanationLabel setFont:[UIFont systemFontOfSize:24.f]];
+    [self.explanationLabel setTextAlignment:NSTextAlignmentCenter];
+    [self.view addSubview:self.explanationLabel];
+
+    // Start Button
+    self.startButton = [[UIButton alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.view.frame) - BUTTON_HEIGHT, CGRectGetWidth(self.view.frame), BUTTON_HEIGHT)];
+    [self.startButton setTitle:@"Start Go/No-Go" forState:UIControlStateNormal];
+    [self.startButton setBackgroundColor:[UIColor colorWithRed:52.0/255 green:73.0/255 blue:94.0/255 alpha:1.0]]; //rgba(52, 73, 94,1.0)
+    [self.startButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.startButton addTarget:self action:@selector(startTest) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.startButton];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
-- (void)viewDidAppear:(BOOL)animated {
-    [self firstRound];
-}
-
-- (void)firstRound {
+- (void)startTest {
+    
+    // Hide button and label
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.startButton setFrame:CGRectMake(0, CGRectGetMaxY(self.view.frame), CGRectGetWidth(self.view.frame), BUTTON_HEIGHT)];
+        [self.explanationLabel setHidden:YES];
+    }];
     
     // Show plus sign for 800ms
     UIImageView *imgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"plus-sign"]];
@@ -68,7 +99,13 @@
                         
                         // Wait 700ms to start again
                         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.7 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                            // ...
+                            
+                            // Show button and label
+                            [UIView animateWithDuration:0.1 animations:^{
+                                [self.startButton setFrame:CGRectMake(0, CGRectGetMaxY(self.view.frame) - BUTTON_HEIGHT, CGRectGetWidth(self.view.frame), BUTTON_HEIGHT)];
+                                [self.explanationLabel setHidden:NO];
+                            }];
+                            
                         });
                     });
                     
