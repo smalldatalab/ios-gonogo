1af442cc222b99c0810fb3a0a07742a7a7452bae
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2016-01-15 16:04:19 -0500

    Added logic for earnings & potential gain in BART

--- Go-No-Go/Go-No-Go/BalloonViewController.m
+++ Go-No-Go/Go-No-Go/BalloonViewController.m
@@ -11,6 +11,11 @@
 @interface BalloonViewController ()
 
 @property (nonatomic, strong) UIImageView *balloon;
+@property (nonatomic, strong) IBOutlet UILabel *potentialGainLabel;
+@property (nonatomic, strong) IBOutlet UILabel *totalEarningsLabel;
+
+@property (nonatomic, assign) double earnings;  // so far
+@property (nonatomic, assign) double gains;     // on current pumping
 
 @end
 
@@ -23,9 +28,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // Done button to dismiss views
     UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(dismissView)];
     [item setTintColor:[UIColor colorWithRed:52.0/255 green:73.0/255 blue:94.0/255 alpha:1.0]];
     [self.navigationItem setRightBarButtonItem:item];
+    
+    // Data
+    self.earnings = 0;
+    self.gains = 0;
+    [self updateEarningLabels];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -34,6 +45,7 @@
     self.balloon = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"balloon"]];
     self.balloon.center = self.view.center;
     [self.view addSubview:self.balloon];
+    [self.view sendSubviewToBack:self.balloon];
 }
 
 - (void)dismissView
@@ -61,12 +73,31 @@
     // (Slightly more on the x-axis to mimic real balloon)
     CGAffineTransform transform = CGAffineTransformScale(self.balloon.transform, 1.055, 1.05);
     [self.balloon setTransform:transform];
+    
+    // Update data
+    self.gains += 0.05;
+    [self updateEarningLabels];
 }
 
 - (IBAction)tappedCollect:(id)sender
 {
     // Reset balloon size
     [self.balloon setTransform:CGAffineTransformIdentity];
+    
+    // Update data
+    self.earnings += self.gains;
+    self.gains = 0;
+    [self updateEarningLabels];
+}
+
+//------------------------------------------------------------------------------------------
+#pragma mark - Data -
+//------------------------------------------------------------------------------------------
+
+- (void)updateEarningLabels
+{
+    self.totalEarningsLabel.text = [NSString stringWithFormat:@"Total Earnings: $%.2f", self.earnings];
+    self.potentialGainLabel.text = [NSString stringWithFormat:@"Potential Gain: $%.2f", self.gains];
 }
 
 @end
