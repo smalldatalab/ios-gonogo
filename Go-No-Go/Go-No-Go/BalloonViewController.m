ac62748d27123df95f52055eb43994d30e8f035e
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2016-01-26 11:55:53 -0500

    Added balloon implosion animation

--- Go-No-Go/Go-No-Go/BalloonViewController.m
+++ Go-No-Go/Go-No-Go/BalloonViewController.m
@@ -7,15 +7,24 @@
 //
 
 #import "BalloonViewController.h"
+#import "UIView+Explode.h"
+
+static CGFloat const kPumpFactor  = 1.1;
+static CGFloat const kGainPerPump = 0.25;
+static NSInteger const kMaxPumps  = 12;
 
 @interface BalloonViewController ()
 
 @property (nonatomic, strong) UIImageView *balloon;
 @property (nonatomic, strong) IBOutlet UILabel *potentialGainLabel;
 @property (nonatomic, strong) IBOutlet UILabel *totalEarningsLabel;
+@property (nonatomic, strong) IBOutlet UIButton *pumpButton;
+@property (nonatomic, strong) IBOutlet UIButton *collectButton;
 
-@property (nonatomic, assign) double earnings;  // so far
-@property (nonatomic, assign) double gains;     // on current pumping
+@property (nonatomic, assign) double earnings;
+@property (nonatomic, assign) double potentialGain;
+@property (nonatomic, assign) int pumps;
+@property (nonatomic, assign) int positionToImplode;
 
 @end
 
@@ -35,30 +44,19 @@
     
     // Data
     self.earnings = 0;
-    self.gains = 0;
-    [self updateEarningLabels];
+    self.potentialGain = 0;
+    self.pumps = 0;
+    self.positionToImplode = kMaxPumps + 1;
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    
-    // Balloon image
-    self.balloon = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"balloon"]];
-    
-    // Center it, with slight y-axis offset
-    self.balloon.center   = self.view.center;
-    CGRect balloonFrame   = self.balloon.frame;
-    balloonFrame.origin.y -= CGRectGetHeight(self.potentialGainLabel.frame);
-    self.balloon.frame    = balloonFrame;
 
-    // Add to view, but below all others
-    [self.view addSubview:self.balloon];
-    [self.view sendSubviewToBack:self.balloon];
+    // Set up the balloon on the screen
+    [self resetBalloon];
 }
 
-- (void)dismissView
-{
+- (void)dismissView {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
@@ -70,44 +68,92 @@
 #pragma mark - Actions -
 //------------------------------------------------------------------------------------------
 
-- (IBAction)tappedPump:(id)sender
-{
-    // Prevent the balloon from becoming too large for the screen
-    if (CGRectGetWidth(self.balloon.frame) + 30 >= CGRectGetWidth(self.view.frame) ||
-        CGRectGetMinY(self.balloon.frame) <= 0 ||
-        CGRectGetMaxY(self.balloon.frame) + 8 >= CGRectGetMinY(self.potentialGainLabel.frame)) {
+- (IBAction)tappedPump:(id)sender {
+    // Increment current number of pumps
+    self.pumps++;
+    
+    // Balloon imploding now
+    if (self.pumps == self.positionToImplode || self.pumps >= kMaxPumps) {
+        [self implodeBalloon];
         return;
     }
+
+    // Increase the factor a little, proportionally to the device's screen size
+    CGFloat increment = (CGRectGetWidth(self.view.frame) / (kMaxPumps * 1000));
     
-    // Inflate balloon by a factor of 1.055,1.05
-    // (Slightly more on the x-axis to mimic real balloon)
-    CGAffineTransform transform = CGAffineTransformScale(self.balloon.transform, 1.055, 1.05);
-    [self.balloon setTransform:transform];
+    // Animate pumping
+    [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.6 initialSpringVelocity:0.6 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        // Inflate slightly more on the x-axis to mimic real balloon
+        CGAffineTransform transform = CGAffineTransformScale(self.balloon.transform, 1.005 * (kPumpFactor+increment), (kPumpFactor+increment));
+        [self.balloon setTransform:transform];
+    } completion:nil];
     
     // Update data
-    self.gains += 0.05;
+    self.potentialGain += kGainPerPump;
     [self updateEarningLabels];
 }
 
-- (IBAction)tappedCollect:(id)sender
-{
-    // Reset balloon size
-    [self.balloon setTransform:CGAffineTransformIdentity];
-    
-    // Update data
-    self.earnings += self.gains;
-    self.gains = 0;
-    [self updateEarningLabels];
+- (IBAction)tappedCollect:(id)sender {
+    [self resetBalloon];
 }
 
 //------------------------------------------------------------------------------------------
 #pragma mark - Data -
 //------------------------------------------------------------------------------------------
 
-- (void)updateEarningLabels
-{
+- (void)implodeBalloon {
+    // No gains if balloon implodes
+    self.potentialGain = 0;
+    [self updateEarningLabels];
+
+    // Prevent user pumping or collecting during animation
+    [self.view setUserInteractionEnabled:NO];
+    [self.pumpButton setEnabled:NO];
+    [self.collectButton setEnabled:NO];
+    
+    // Explosion animation
+    [self.balloon lp_explodeWithCallback:^{
+        [self resetBalloon];
+        [self.view setUserInteractionEnabled:YES];
+        [self.pumpButton setEnabled:YES];
+        [self.collectButton setEnabled:YES];
+    }];
+}
+
+- (void)resetBalloon {
+    [self.balloon removeFromSuperview];
+    
+    // Balloon image
+    self.balloon = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"balloon"]];
+    
+    // Center it, with slight y-axis offset
+    self.balloon.center   = self.view.center;
+    CGRect balloonFrame   = self.balloon.frame;
+    balloonFrame.origin.y -= (CGRectGetHeight(self.potentialGainLabel.frame) + 20.0);
+    self.balloon.frame    = balloonFrame;
+    
+    // Reset balloon size
+    [self.balloon setTransform:CGAffineTransformIdentity];
+    
+    // Add to view, but below all others
+    [self.view addSubview:self.balloon];
+    [self.view sendSubviewToBack:self.balloon];
+    
+    // Update data
+    self.earnings += self.potentialGain;
+    self.potentialGain = 0;
+    self.pumps = 0;
+    [self pickRandomImplodingStep];
+    [self updateEarningLabels];
+}
+
+- (void)pickRandomImplodingStep {
+    self.positionToImplode = arc4random_uniform(kMaxPumps+1) + 1;
+}
+
+- (void)updateEarningLabels {
     self.totalEarningsLabel.text = [NSString stringWithFormat:@"Total Earnings: $%.2f", self.earnings];
-    self.potentialGainLabel.text = [NSString stringWithFormat:@"Potential Gain: $%.2f", self.gains];
+    self.potentialGainLabel.text = [NSString stringWithFormat:@"Potential Gain: $%.2f", self.potentialGain];
 }
 
 @end
